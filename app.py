e982014830e2cc61eabfb75ad0ba61ae16c9ffeb
YihaoSu <yhsu@astro.ncu.edu.tw>
2022-03-05 13:43:33 +0800

    新增重力波資料範例所需套件

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import (
 	exoplanet,
+	gravity_wave,
 	sun
 )
 
@@ -11,5 +12,6 @@ st.set_page_config(layout="wide")
 
 app = MultiApp()
 app.add_app('太陽系外行星', exoplanet.app)
+app.add_app('重力波', gravity_wave.app)
 app.add_app('太陽', sun.app)
 app.run()
---
+++ apps/gravity_wave.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def app():
+    st.title('取得重力波資料的範例')
